d37ef00aca43e6f18651e3de3963f0440b4aeb7d
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-05-12 15:00:25 +0200

    switch to Sphinx in order to render Markdown table
    Rendering Markdown cell output requires Myst-NB >=0.14, which is not
    yet available with Jupyter Book.

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+import os
+
+
+def get_execution_mode() -> str:
+    if "EXECUTE_NB" in os.environ:
+        print("\033[93;1mWill run Jupyter notebooks!\033[0m")
+        return "force"
+    return "off"
+
+
+author = "Mikhail Mikhasenko, Remco de Boer"
+copyright = "2022"
+exclude_patterns = [
+    "**.ipynb_checkpoints",
+    ".DS_Store",
+    "Thumbs.db",
+    "_build",
+]
+extensions = [
+    "myst_nb",
+    "sphinx.ext.intersphinx",
+    "sphinx_book_theme",
+    "sphinx_copybutton",
+    "sphinx_external_toc",
+    "sphinx_panels",
+    "sphinx_togglebutton",
+]
+external_toc_path = "_toc.yml"
+html_sourcelink_suffix = ""
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+    },
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/redeboer/polarization-sensitivity",
+    "repository_branch": "main",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+}
+html_title = "Polarization sensitivity"
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "substitution",
+]
+nb_execution_allow_errors = False
+nb_execution_mode = get_execution_mode()
+nb_execution_timeout = -1
+nb_output_stderr = "show"
+nb_render_markdown_format = "myst"
+numfig = True
+panels_add_bootstrap_css = False
+pygments_style = "sphinx"
+use_multitoc_numbering = True
