476e1e8b434499ae3150b1f537b43bf64e7cc0c2
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-06-03 10:39:16 +0200

    refactor: return Symbol from formulate_scattering_angle()

--- src/polarization/amplitude.py
+++ src/polarization/amplitude.py
@@ -5,7 +5,9 @@ import sympy as sp
 from polarization.dynamics import Kallen
 
 
-def formulate_scattering_angle(state_id: int, sibling_id: int) -> sp.Expr:
+def formulate_scattering_angle(
+    state_id: int, sibling_id: int
+) -> tuple[sp.Symbol, sp.acos]:
     r"""Formulate the scattering angle in the rest frame of the resonance.
 
     Compute the :math:`\theta_{ij}` scattering angle as formulated in `Eq (A1) in the
@@ -17,6 +19,7 @@ def formulate_scattering_angle(state_id: int, sibling_id: int) -> sp.Expr:
         raise ValueError(f"Child IDs need to be one of 1, 2, 3")
     if state_id == sibling_id:
         raise ValueError(f"IDs of the decay products cannot be equal: {state_id}")
+    symbol = sp.Symbol(Rf"theta_{state_id}{sibling_id})", real=True)
     spectator_id = next(iter({1, 2, 3} - {state_id, sibling_id}))
     m0 = sp.Symbol(f"m0", nonnegative=True)
     mi = sp.Symbol(f"m{state_id}", nonnegative=True)
@@ -24,7 +27,7 @@ def formulate_scattering_angle(state_id: int, sibling_id: int) -> sp.Expr:
     mk = sp.Symbol(f"m{spectator_id}", nonnegative=True)
     σj = sp.Symbol(f"sigma{sibling_id}", nonnegative=True)
     σk = sp.Symbol(f"sigma{spectator_id}", nonnegative=True)
-    return sp.acos(
+    theta = sp.acos(
         (
             2 * σk * (σj - mk**2 - mi**2)
             - (σk + mi**2 - mj**2) * (m0**2 - σk - mk**2)
@@ -34,3 +37,4 @@ def formulate_scattering_angle(state_id: int, sibling_id: int) -> sp.Expr:
             * sp.sqrt(Kallen(σk, mi**2, mj**2))
         )
     )
+    return symbol, theta
--- tests/test_amplitude.py
+++ tests/test_amplitude.py
@@ -8,7 +8,7 @@ m0, m1, m2, m3 = sp.symbols("m:4", nonnegative=True)
 
 
 def test_formulate_scattering_angle():
-    assert formulate_scattering_angle(2, 3) == sp.acos(
+    assert formulate_scattering_angle(2, 3)[1] == sp.acos(
         (
             2 * σ1 * (-(m1**2) - m2**2 + σ3)
             - (m0**2 - m1**2 - σ1) * (m2**2 - m3**2 + σ1)
@@ -18,7 +18,7 @@ def test_formulate_scattering_angle():
             * sp.sqrt(Kallen(σ1, m2**2, m3**2))
         )
     )
-    assert formulate_scattering_angle(3, 1) == sp.acos(
+    assert formulate_scattering_angle(3, 1)[1] == sp.acos(
         (
             2 * σ2 * (-(m2**2) - m3**2 + σ1)
             - (m0**2 - m2**2 - σ2) * (-(m1**2) + m3**2 + σ2)
