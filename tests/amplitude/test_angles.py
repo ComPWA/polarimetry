6faa61e686a3ad56114ae9b1792b5e70d887e2b1
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-06-03 12:16:47 +0200

    refactor: move angle functions to sub-module

--- tests/amplitude/test_angles.py
+++ tests/amplitude/test_angles.py
@@ -2,7 +2,7 @@ import pytest
 import sympy as sp
 from ampform.kinematics.phasespace import Kallen, compute_third_mandelstam
 
-from polarization.amplitude import (
+from polarization.amplitude.angles import (
     formulate_scattering_angle,
     formulate_theta_hat_angle,
     formulate_zeta_angle,
