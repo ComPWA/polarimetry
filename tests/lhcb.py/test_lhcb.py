acc743f75cec92ec15c5113c9d8beaaa723a70ae
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-11-22 00:15:29 +0100

    MAINT: add test for `get_conversion_factor_ls()`

--- tests/lhcb.py/test_lhcb.py
+++ tests/lhcb.py/test_lhcb.py
@@ -2,27 +2,57 @@ from __future__ import annotations
 
 from os.path import dirname
 
-from polarimetry.lhcb import load_model_builder, load_model_parameters
+from polarimetry.amplitude import DalitzPlotDecompositionBuilder
+from polarimetry.lhcb import (
+    get_conversion_factor_ls,
+    load_model_builder,
+    load_model_parameters,
+)
 from polarimetry.lhcb.particle import load_particles
 
 THIS_DIR = dirname(__file__)
 DATA_DIR = f"{THIS_DIR}/../../data"
+MODEL_FILE = f"{DATA_DIR}/model-definitions.yaml"
 
 
-def _load_parameters(model_choice: int | str) -> dict[str, complex | float | int]:
-    model_file = f"{DATA_DIR}/model-definitions.yaml"
-    particles = load_particles(f"{DATA_DIR}/particle-definitions.yaml")
-    model_builder = load_model_builder(model_file, particles, model_choice)
-    symbol_parameters = load_model_parameters(
-        filename=model_file,
-        decay=model_builder.decay,
-        model_id=model_choice,
-    )
-    parameters = {str(par): value for par, value in symbol_parameters.items()}
-    print()
-    print("Run pytest -s to view these parameter names:")
-    print("  ", "\n  ".join(sorted(parameters)))
-    return parameters
+def test_get_conversion_factor_ls():
+    builder = _load_builder("Alternative amplitude model obtained using LS couplings")
+    decay = builder.decay
+    items = [
+        f"{c.resonance.name:9s}"
+        f"L={c.incoming_ls.L!r:3s}"
+        f"S={c.incoming_ls.S!r:5s}"
+        f"factor={get_conversion_factor_ls(c.decay):+d}"
+        for c in decay.chains
+    ]
+    assert items == [
+        "L(1405)  L=0  S=1/2  factor=-1",
+        "L(1405)  L=1  S=1/2  factor=+1",
+        "L(1520)  L=1  S=3/2  factor=-1",
+        "L(1520)  L=2  S=3/2  factor=+1",
+        "L(1600)  L=0  S=1/2  factor=-1",
+        "L(1600)  L=1  S=1/2  factor=+1",
+        "L(1670)  L=0  S=1/2  factor=-1",
+        "L(1670)  L=1  S=1/2  factor=+1",
+        "L(1690)  L=1  S=3/2  factor=-1",
+        "L(1690)  L=2  S=3/2  factor=+1",
+        "L(2000)  L=0  S=1/2  factor=-1",
+        "L(2000)  L=1  S=1/2  factor=+1",
+        "D(1232)  L=1  S=3/2  factor=-1",
+        "D(1232)  L=2  S=3/2  factor=+1",
+        "D(1600)  L=1  S=3/2  factor=-1",
+        "D(1600)  L=2  S=3/2  factor=+1",
+        "D(1700)  L=1  S=3/2  factor=-1",
+        "D(1700)  L=2  S=3/2  factor=+1",
+        "K(700)   L=0  S=1/2  factor=-1",
+        "K(700)   L=1  S=1/2  factor=+1",
+        "K(892)   L=0  S=1/2  factor=-1",
+        "K(892)   L=1  S=1/2  factor=+1",
+        "K(892)   L=1  S=3/2  factor=-1",
+        "K(892)   L=2  S=3/2  factor=+1",
+        "K(1430)  L=0  S=1/2  factor=-1",
+        "K(1430)  L=1  S=1/2  factor=+1",
+    ]
 
 
 def test_load_model_parameters():
@@ -54,3 +84,22 @@ def test_load_model_parameters():
         parameters[R"\mathcal{H}^\mathrm{production}[L(1810), 1/2, 0]"]
         == -1.179995 - 4.413438j  # conversion factor -1
     )
+
+
+def _load_builder(model_choice: int | str) -> DalitzPlotDecompositionBuilder:
+    particles = load_particles(f"{DATA_DIR}/particle-definitions.yaml")
+    return load_model_builder(MODEL_FILE, particles, model_choice)
+
+
+def _load_parameters(model_choice: int | str) -> dict[str, complex | float | int]:
+    model_builder = _load_builder(model_choice)
+    symbol_parameters = load_model_parameters(
+        filename=MODEL_FILE,
+        decay=model_builder.decay,
+        model_id=model_choice,
+    )
+    parameters = {str(par): value for par, value in symbol_parameters.items()}
+    print()
+    print("Run pytest -s to view these parameter names:")
+    print("  ", "\n  ".join(sorted(parameters)))
+    return parameters
