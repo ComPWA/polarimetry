8f30fa8eeeecdb976b06dd757698241dc99fe73d
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-19 14:34:56 +0100

    MAINT: run `linkcheck` on https://github.com/ComPWA/polarimetry
    No need to ignore anymore now that it is public

--- docs/conf.py
+++ docs/conf.py
@@ -434,7 +434,6 @@ latex_show_pagerefs = True
 linkcheck_ignore = [
     "https://arxiv.org/pdf/2208.03262.pdf",
     "https://arxiv.org/pdf/hep-ex/0510019.pdf",
-    "https://github.com/ComPWA/polarimetry",
     "https://journals.aps.org/prd/pdf/10.1103/PhysRevD.101.034033",
 ]
 myst_enable_extensions = [
