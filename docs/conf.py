9462090699ac5f0ab52687149285cd888841fdf3
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-10-09 18:10:18 +0200

    MAINT: update pip constraints and pre-commit (#314)
    * DOC: make Colab TOC visible by default
    * DX: lint PRs with shared commitlint config
    * DX: merge `setup.cfg` into `pyproject.toml`
    * DX: switch to `black-jupyter` hook
    * DX: remove `.prettierrc`
    * DX: remove GitHub Issue templates
    * DX: synchronize ComPWA dev environment
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -244,11 +244,13 @@ def get_version(package_name: str) -> str:
     version_remapping = {
         "ipython": {
             "8.12.2": "8.12.1",
+            "8.12.3": "8.12.1",
         },
         "ipywidgets": {
             "8.0.3": "8.0.5",
             "8.0.4": "8.0.5",
             "8.0.6": "8.0.5",
+            "8.1.1": "8.1.2",
         },
     }
     for line in constraints.split("\n"):
