b936dde9b18449640b572d42de1ac59c806cf81f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-03-19 17:57:29 +0100

    FIX: relink itersphinx to ampform-dpd

--- src/polarimetry/lhcb/particle.py
+++ src/polarimetry/lhcb/particle.py
@@ -11,7 +11,7 @@ from ampform_dpd.decay import Particle
 
 
 def load_particles(filename: Path | str) -> dict[str, Particle]:
-    """Load `.Particle` definitions from a YAML file."""
+    """Load `~ampform_dpd.decay.Particle` definitions from a YAML file."""
     particle_definitions = _load_particles_json(filename)
     return _to_resonance_dict(particle_definitions)
 
