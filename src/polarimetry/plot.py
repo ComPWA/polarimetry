d2d75d8d542396058ffb2b0703948101ef30c481
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-12-20 22:41:05 +0100

    BREAK: remove `stylize_contour()`

--- src/polarimetry/plot.py
+++ src/polarimetry/plot.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import shutil
-from importlib.metadata import version
 from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
@@ -11,7 +10,6 @@ import matplotlib.pyplot as plt
 if TYPE_CHECKING:
     from matplotlib.artist import Artist
     from matplotlib.axes import Axes
-    from matplotlib.collections import Collection
     from matplotlib.contour import QuadContourSet
 
 
@@ -50,25 +48,3 @@ def _wake_up_matplotlib() -> None:
     """Somehow `plt.rc` does not work if a figure hasn't been created before..."""
     plt.figure()
     plt.close()
-
-
-def stylize_contour(
-    contour_set: QuadContourSet,
-    *,
-    edgecolor=None,
-    label: str | None = None,
-    linestyle: str | None = None,
-    linewidth: float | None = None,
-) -> None:
-    if version("matplotlib") >= "3.8.0":
-        contour: Collection = contour_set
-    else:
-        contour = contour_set.collections[0]
-    if edgecolor is not None:
-        contour.set_edgecolor(edgecolor)
-    if label is not None:
-        contour.set_label(label)
-    if linestyle is not None:
-        contour.set_linestyle(linestyle)
-    if linewidth is not None:
-        contour.set_linewidth(linewidth)
