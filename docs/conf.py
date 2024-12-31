ee17846b7cd562642d7be11d47aacfcade2a78e5
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-12-31 15:27:57 +0100

    FIX: update linkcheck ignore list

--- docs/conf.py
+++ docs/conf.py
@@ -451,6 +451,7 @@ linkcheck_ignore = [
     "https://arxiv.org/pdf/2208.03262.pdf",
     "https://arxiv.org/pdf/hep-ex/0510019.pdf",
     "https://doi.org/10.1103/PhysRevD.101.034033",
+    "https://indico.cern.ch/event/1187317",
     "https://journals.aps.org/prd/pdf/10.1103/PhysRevD.101.034033",
     "https://www.bookfinder.com",
 ]
