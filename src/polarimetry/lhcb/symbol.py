4d5bab8b6623ce5fd053a7b24c0f8792918e844f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-10-20 11:24:59 +0200

    BEHAVIOR: use LaTeX in dynamics symbol names

--- src/polarimetry/lhcb/symbol.py
+++ src/polarimetry/lhcb/symbol.py
@@ -21,7 +21,7 @@ class Particle(Protocol):
 def create_mass_symbol(particle: Particle | State | str) -> sp.Symbol:
     if isinstance(particle, State):
         return sp.Symbol(f"m{particle.index}", nonnegative=True)
-    particle = _get_name(particle)
+    particle = _get_latex(particle)
     return sp.Symbol(f"m_{{{particle}}}", nonnegative=True)
 
 
@@ -29,7 +29,7 @@ def create_width_symbol(
     particle: str | Particle,
     decay_products: tuple[str | Particle, str | Particle] | None = None,
 ) -> sp.Symbol:
-    particle = _get_name(particle)
+    particle = _get_latex(particle)
     suffix = ""
     if decay_products:
         p1, p2 = map(_get_latex, decay_products)
@@ -50,23 +50,17 @@ def create_meson_radius_symbol(typ: Literal["production", "decay"]) -> sp.Symbol
 
 def create_alpha_symbol(particle: str | Particle) -> sp.Symbol:
     """Define symbol :math:`alpha` for `this paper<https://arxiv.org/pdf/hep-ex/0510019.pdf>`_."""
-    particle = _get_name(particle)
+    particle = _get_latex(particle)
     return sp.Symbol(Rf"\alpha_{{{particle}}}", real=True)
 
 
 def create_gamma_symbol(particle: str | Particle) -> sp.Symbol:
     """Define symbol :math:`gamma` for `this paper<https://arxiv.org/pdf/hep-ex/0510019.pdf>`_."""
-    particle = _get_name(particle)
+    particle = _get_latex(particle)
     return sp.Symbol(Rf"\gamma_{{{particle}}}", nonnegative=True)
 
 
 def _get_latex(particle: str | Particle) -> str:
     if latex := getattr(particle, "latex", None):
         return latex
-    return str(particle)
-
-
-def _get_name(particle: str | Particle) -> str:
-    if name := getattr(particle, "name", None):
-        return name
     return str(particle)
